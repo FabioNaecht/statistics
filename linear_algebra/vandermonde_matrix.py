9d730b52dcfccd87102645469885a916419c65d0
FabioNaecht <103195215+FabioNaecht@users.noreply.github.com>
2023-12-29 12:10:04 +0100

    optimized code

--- linear_algebra/vandermonde_matrix.py
+++ linear_algebra/vandermonde_matrix.py
@@ -12,28 +12,29 @@ print(f"Vandermond matrix:\n{vandermond_matrix}\n")
 # solve the system of linear equations
 coefficients = np.linalg.solve(vandermond_matrix, y_coordinates)
 print(f"coefficients:\n{coefficients}\n")
+coefficients_reverse = np.flip(coefficients)
 
 # determine polynomial function from coefficients with lamda
 polynomial_function = lambda x: coefficients[-1] * x ** 5 + coefficients[-2] * x ** 4 + coefficients[-3] * x ** 3 + \
                                     coefficients[-4] * x ** 2 + coefficients[-5] * x + coefficients[-6]
-
 # determine a spline function from the coordinates
 spline_function = CubicSpline(x_coordinates, y_coordinates)
 
-# determine bezier curve fit
-# bezier_curve = bezier.Curve(coordinates, degree=5)
-
-
 # plot coordinates and polynomial fit
 x = np.linspace(min(x_coordinates), max(y_coordinates), 100)
-plt.plot(x_coordinates, y_coordinates, 'o')
+
+# avoiding lamda expression:
+polynomial_function_np = np.polyval(coefficients_reverse, x)
+
+plt.plot(x_coordinates, y_coordinates, 'o', label='coordinates')
 # plot the polynomial function
-plt.plot(x, polynomial_function(x))
+plt.plot(x, polynomial_function(x), label='Vandermonde matrix fit')
+plt.plot(x, polynomial_function_np, '--', color='navy', label='Vandermonde matrix fit numpy')
 # plot the spline function
-plt.plot(x, spline_function(x), '--')
+plt.plot(x, spline_function(x), '--', color='red', label='spline fit')
 plt.title('Trajectory Fitting')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.legend(['data', 'Vandermonde matrix fit', 'cubic spline fit'])
+plt.legend()
 
 plt.show()
